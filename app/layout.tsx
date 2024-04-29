05f57ca62c693de9a81bf12f9f409feca98fa4ec
Arjun-Seth <arjun.drag007@gmail.com>
2024-04-29 18:15:51 +0530

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Navbar from "@/components/main/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Personal Portfolio",
+  title: "Arjun Portfolio",
   description: "This is my portfolio",
 };
 
